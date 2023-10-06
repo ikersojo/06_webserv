22f852612d0eea0a8885a16978963b505661754b
Andoni Arrien Martin <aarrien-@c3r6s5.42urduliz.com>
2023-10-06 14:21:38 +0200

    locations correctly saved

--- lib/classes/inc/Config.hpp
+++ lib/classes/inc/Config.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Config.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 21:17:18 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/09/27 22:39:29 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:16:49 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@
 
 	#include "../../aux/inc/aux.hpp"
 
+	struct Location {
+		std::string	path;
+		bool		autoindex;
+		std::string	file;
+		bool		allowedGET;
+		bool		allowedPOST;
+		bool		allowedDELETE;
+		std::string	errorPage;
+		int			bufferSize;
+	};
+
 	class Config
 	{
 		public:
@@ -30,18 +41,34 @@
 			~Config(void);
 
 			size_t			getMaxPorts(void);
+			std::string		getServerName(size_t i);
 			int				getPort(size_t i);
 			std::string		getAddress(size_t i);
+			bool			getAutoIndex(size_t i, std::string req);
 			std::string		getFile(size_t i, std::string req);
+			bool			getAllowedGET(size_t i, std::string req);
+			bool			getAllowedPOST(size_t i, std::string req);
+			bool			getAllowedDELETE(size_t i, std::string req);
+			std::string		getErrorPage(size_t i, std::string req);
+			int				getBufferSize(size_t i, std::string req);
+
+			void			resizeVectors(size_t size);
 			void			printConfig(void);
 
 		private:
 			Config(void);
 
-			size_t							_maxPorts;
-			std::vector < int >				_ports;
-			std::vector < std::string >		_addresses;
-			std::vector < std::map < std::string, std::string > >	_files;
+			size_t													_maxPorts;
+			std::vector < std::string >								_servername;
+			std::vector < int >										_port;
+			std::vector < std::string >								_address;
+			std::vector < std::map < std::string, bool > >			_autoindex;
+			std::vector < std::map < std::string, std::string > >	_file;
+			std::vector < std::map < std::string, bool > >			_allowedGET;
+			std::vector < std::map < std::string, bool > >			_allowedPOST;
+			std::vector < std::map < std::string, bool > >			_allowedDELETE;
+			std::vector < std::map < std::string, std::string > >	_errorPage;
+			std::vector < std::map < std::string, int > >			_bufferSize;
 	};
 
 #endif // CONFIG_HPP
--- lib/classes/src/Config.cpp
+++ lib/classes/src/Config.cpp
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   Config.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isojo-go <isojo-go@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 21:33:58 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/10/02 08:51:34 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/10/06 14:15:30 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Config.hpp"
 
-size_t	getNumberOfPorts(const std::string & configFile)
-{
+size_t	getNumberOfPorts(const std::string & configFile) {
 	std::ifstream	inFile(configFile);
 	std::string		line;
 	size_t			n;
@@ -30,20 +29,79 @@ size_t	getNumberOfPorts(const std::string & configFile)
 			n++;
 	}
 	inFile.close();
-	if (DEBUG == 1)
-		std::cout << GREY << "[DEBUG: " << n << " ports to listen to identified]"
-					<< DEF_COL << std::endl;
+	debug("ports to listen to identified");
 	return (n);
 }
 
+
+void Config::resizeVectors(size_t size) {
+	this->_maxPorts = size;
+	(this->_autoindex.resize(size));
+	this->_file.resize(size);
+	this->_allowedGET.resize(size);
+	this->_allowedPOST.resize(size);
+	this->_allowedDELETE.resize(size);
+	this->_errorPage.resize(size);
+	this->_bufferSize.resize(size);
+}
+
+Location	createLocation(std::ifstream& inFile, std::string& line) {
+	Location location = {
+		.path = "",
+		.autoindex = false,
+		.file = "./www/default.html",
+		.allowedGET = true,
+		.allowedPOST = true,
+		.allowedDELETE = true,
+		.errorPage = "./www/404.html",
+		.bufferSize = 4096,
+	};
+	if (line.find("location:") != std::string::npos) {
+		// configuración específica por location
+		location.path = trimChars(line.substr(line.find(":")+1), " \"");
+		while (getline(inFile, line) && line.find("location:") == std::string::npos) {
+			if (line.find("root:") != std::string::npos)
+				location.file = trimChars(line.substr(line.find(":")+1), " \"") + location.path;
+			if (line.find("file:") != std::string::npos) {
+				location.file += ( "/" + trimChars(line.substr(line.find(":")+1), " \""));
+			}
+			if (line.find("autoindex:") != std::string::npos)
+				if (line.find("on") != std::string::npos) location.autoindex = true;
+			if (line.find("allow:") != std::string::npos) {
+				if (line.find("GET") == std::string::npos) location.allowedGET = false;
+				if (line.find("POST") == std::string::npos) location.allowedPOST = false;
+				if (line.find("DELETE") == std::string::npos) location.allowedDELETE = false;
+			}
+			if (line.find("error_page") != std::string::npos)
+				location.errorPage = trimChars(line.substr(line.find(":")+1), " \"");
+			if (line.find("buffer_size") != std::string::npos)
+				location.bufferSize = std::atoi((trimChars(line.substr(line.find(":")+1), " \"")).c_str());
+		}
+	}
+	return (location);
+}
+
+void	printLocations(std::vector<Location>& locations) {
+	std::cout << "\nLOCATIONS:\n";
+	for (std::vector<Location>::iterator it = locations.begin(); it != locations.end();it++) {
+		std::cout << "location " << it->path << ":" << std::endl;
+		std::cout << "  file:        " << it->file << std::endl;
+		std::cout << "  autoindex:   " << (it->autoindex ? "on" : "off") << std::endl;
+		std::cout << "  allow:       ";
+		if (it->allowedGET) std::cout << "GET ";
+		if (it->allowedPOST) std::cout << "POST ";
+		if (it->allowedDELETE) std::cout << "DELETE ";
+		std::cout << std::endl;
+		std::cout << "  error_page:  " << it->errorPage << std::endl;
+		std::cout << "  buffer_size: " << it->bufferSize << std::endl;
+	}
+}
 
 Config::Config(const std::string & configFile)
 {
 	std::cout << "Loading " << configFile << "..." << std::endl << std::endl;
 
-	std::string line, item1, item2, item3, rootDir, path, fullPath;
-	int	nPortsPerServer;
-	int	nPortsPopulated = 0;
+	std::string line;
 	std::ifstream inFile(configFile);
 	if (!inFile.is_open())
 	{
@@ -51,132 +109,85 @@ Config::Config(const std::string & configFile)
 		exit(EXIT_FAILURE);
 	}
 
-	this->_maxPorts = getNumberOfPorts(configFile);
-	this->_files.resize(this->_maxPorts);
+	resizeVectors(getNumberOfPorts(configFile));
 
-	while (getline(inFile, line))
-	{
-		std::istringstream iss(line);
-		if (iss >> item1 && item1 == "server:")
-		{
-
-			while (getline(inFile, line))
-			{
-				if (line.find("listen:") != std::string::npos)
-				{
-					nPortsPerServer = 0;
-					while (getline(inFile, line) && 
-							((line.find("port:") != std::string::npos) || (line.find("address:") != std::string::npos)))
-					{
-						std::istringstream issPort(line);
-						issPort >> item1 >> item2 >> item3;
-						if (item1 == "-" && item2 == "port:")
-						{
-							// std::cout << "PORT: " << item3 << std::endl; // do things with port
-							nPortsPerServer++;
-							this->_ports.push_back(atoi(item3.c_str()));
-
-							if (getline(inFile, line) && line.find("address:") != std::string::npos)
-							{
-								std::istringstream issAddress(line);
-								issAddress >> item2 >> item3;
-								// std::cout << "ADDRESS: " << item3 << std::endl; // do things with address
-								this->_addresses.push_back(trimDoubleQuotes(item3));
-							}
-						}
-					}
-				}
-				if (line.find("root_directory:") != std::string::npos)
-				{
-					std::istringstream issRootDir(line);
-					issRootDir >> item1 >> item2;
-					rootDir = trimDoubleQuotes(item2);
-					rootDir+= "/";
-					// std::cout << "ROOT DIR: " << rootDir << std::endl; // do things with root dir
-				}
-				if (line.find("locations:") != std::string::npos)
-				{
-					while (getline(inFile, line) && 
-							((line.find("path:") != std::string::npos) || (line.find("file:") != std::string::npos)))
-					{
-						std::istringstream issPath(line);
-						issPath >> item1 >> item2 >> item3;
-						if (item1 == "-" && item2 == "path:")
-						{
-							path = trimDoubleQuotes(item3);
-							// std::cout << "PATH: " << path << std::endl; // do things with path
-
-							if (getline(inFile, line) && line.find("file:") != std::string::npos)
-							{
-								std::istringstream issFile(line);
-								issFile >> item2 >> item3;
-								fullPath = rootDir;
-								fullPath+= trimDoubleQuotes(item3);
-								// std::cout << "FILE: " << fullPath << std::endl; // do things with file
-							}
-						}
-						int	i = -1;
-						while (++i < nPortsPerServer)
-							this->_files[nPortsPopulated + i].insert(std::make_pair(path, fullPath));
-					}
-					nPortsPopulated += nPortsPerServer;
-				}
-			}
-		}
+	std::vector<Location> locations;
+	while (line.find("location:") != std::string::npos || getline(inFile, line)) {
+		if (line.find("location:") == std::string::npos)
+			continue;
+		Location location = createLocation(inFile, line);
+		locations.push_back(location);
 	}
+	printLocations(locations);
 	inFile.close();
 	debug("Config Object Created");
 }
 
 
-Config::~Config(void)
-{
+Config::~Config(void) {
 	debug("Config Object Destroyed");
 }
 
 
-size_t	Config::getMaxPorts(void)
-{
-	return (this->_maxPorts);
-}
+size_t			Config::getMaxPorts(void) { return (this->_maxPorts); }
+
+std::string		Config::getServerName(size_t i) { return (this->_servername[i]); }
+
+int				Config::getPort(size_t i) { return (this->_port[i]); }
+
+std::string		Config::getAddress(size_t i) { return (this->_address[i]); }
+
+bool			Config::getAutoIndex(size_t i, std::string req) { return (this->_autoindex[i][req]); }
+
+std::string		Config::getFile(size_t i, std::string req) { return (this->_file[i][req]); }
+
+bool			Config::getAllowedGET(size_t i, std::string req) { return (this->_allowedGET[i][req]); }
+
+bool			Config::getAllowedPOST(size_t i, std::string req) { return (this->_allowedPOST[i][req]); }
+
+bool			Config::getAllowedDELETE(size_t i, std::string req) { return (this->_allowedDELETE[i][req]); }
+
+std::string		Config::getErrorPage(size_t i, std::string req) { return (this->_errorPage[i][req]); }
+
+int				Config::getBufferSize(size_t i, std::string req) { return (this->_bufferSize[i][req]); }
 
 
-int		Config::getPort(size_t i)
-{
-	return (this->_ports[i]);
-}
-
-
-std::string		Config::getAddress(size_t i)
-{
-	return (this->_addresses[i]);
-}
-
-
-std::string		Config::getFile(size_t i, std::string req)
-{
-	return (this->_files[i][req]);
-}
-
-
-void	Config::printConfig(void)
-{
+void	Config::printConfig(void) {
 	debug("Printing config...");
 	size_t	i = 0;
 	std::cout << GREY << std::endl << "------ CONFIG FILE INPUT (DEBUG) ------" << std::endl;
 	while (i < this->_maxPorts)
 	{
 		std::cout << std::endl << "  LISTENING LOCATION " << i << ":" << std::endl;
-		std::cout << "    http://" << this->_addresses[i] << ":" << this->_ports[i] << std::endl;
+		//std::cout << "    Server name :  " << this->_servername[i] << std::endl;
+		std::cout << "    Listening on : " << this->_address[i] << ":" << this->_port[i] << std::endl;
 
-		std::map<std::string, std::string>& currentMap = this->_files[i];
-		std::map<std::string, std::string>::iterator it = currentMap.begin();
-		while ( it != currentMap.end())
+		std::map<std::string, std::string>& tempMap = this->_file[i];
+		std::map<std::string, std::string>::iterator it = tempMap.begin();
+		while ( it != tempMap.end())
 		{
-			std::cout << "    Requests to " << it->first << " will be replied by " << it->second << std::endl;
+			std::cout << "    Request to route " << it->first << " :" << std::endl;
+			//std::cout << "      autoindex : " << _autoindex[i][it->first] << std::endl;
+			std::cout << "      file :      " << _file[i][it->first] << std::endl;
 			it++;
 		}
 		i++;
 	}
 	std::cout << DEF_COL << std::endl;
 }
+
+
+
+/*for (std::vector<Location>::iterator it = locations.begin(); it != locations.end();i++) {
+	std::cout << "LOCATION (" << &it << ")\n";
+	std::cout << "location " << it->path << ":" << std::endl;
+	std::cout << "  file:        " << it->file << std::endl;
+	std::cout << "  autoindex:   " << it->autoindex << std::endl;
+	std::cout << "  allow:       ";
+	if (it->allowedGET) std::cout << "GET ";
+	if (it->allowedPOST) std::cout << "POST ";
+	if (it->allowedDELETE) std::cout << "DELETE ";
+	std::cout << std::endl;
+	std::cout << "  error_page:  " << it->errorPage << std::endl;
+	std::cout << "  buffer_size: " << it->bufferSize << std::endl;
+} */
