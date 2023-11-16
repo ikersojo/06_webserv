2612def6cdbbf9908be572d5da8d9334573e823b
Iker Sojo <ikersojo@hotmail.com>
2023-11-16 15:29:24 +0100

    update

--- lib/classes/src/ResponseBuilder_03_hPOST_00-main.cpp
+++ lib/classes/src/ResponseBuilder_03_hPOST_00-main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ResponseBuilder_03_hPOST_00-main.cpp               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isojo-go <isojo-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 09:16:19 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/11/16 13:15:36 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:26:02 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ std::string	ResponseBuilder::postResponse(void)
 		error("POST is not allowed");
 		return(this->errorResponse(405));
 	}
-	std::cout << this->_requestStr << std::endl;
 	this->decodeChuncked();
 
 	if (this->_config->isCgi(this->_configIndex, this->_requestParams[1]))
