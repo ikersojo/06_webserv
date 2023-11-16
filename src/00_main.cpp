7879e9b76f5a14f6277263098575267d51c0e656
Andoni Arrien Martin <aarrien-@c4r8s4.42urduliz.com>
2023-11-16 15:49:02 +0100

    todo deleted

--- src/00_main.cpp
+++ src/00_main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_main.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:53:16 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/11/16 14:45:57 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:48:37 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,32 +36,3 @@ int		main(int argc, const char ** argv, char ** envp)
 	}
 	return (0);
 }
-
-/*
-TODO List:
-
-CHECKS:
-- siempre tiene que haber una location "/"
-
-CGI:
-- Diferneciar GET y POST
-- Extraer parametros (tanto para get como post) -->  a un vector
-- Diferenciar extensiones
-- Construir argv para lanzar execve
-
-Pendiente:
-
-	Implement uploads
-	Limit client body size (for POST)
-	Chunked POST requests
-
-	Implement CGI:
-	- use the full path as PATH_INFO
-	- CGI with the file requested as first argument
-	- run in the correct directory for relative path file access
-	- CGI will expect EOF as end of the body
-	- If no content_length is returned from the CGI, EOF will mark the end of the returned data
-
-	Cookies
-
- */
